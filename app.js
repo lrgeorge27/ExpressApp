e7a43da13f3001b986f0aaa368a66e6512583390
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-21 15:59:00 +0000

    finished Express level 3 assignment

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function(request, response) {
-//     response.send('Hello World');
-// });
+var cities = {
+    'Providence': 'Rhode Island',
+    'Omaha': 'Nebraska',
+    'Chicago': 'Illinois',
+    'Seattle': 'Washington',
+    'Boston': 'Massachusetts',
+    'Denver': 'Colorado',
+    'Tulsa': 'Oklahoma',
+    // 'Neworleans': 'Louisiana',
+    // 'Sanfrancisco': 'California',
+    // 'Newyorkcity': 'New York'
+};
 
-// app.get('/name', function(request, response) {
-//     var name = "Lauren George";
-//     response.send(name);
-// });
+var length = Object.keys(cities).length; //find the length of the object, needs global scope
 
-// app.get('/redirect', function(request, response) {
-//     // var redirect = "Surprise, I've moved!";
-//     response.redirect(301, '/surprise');
-// });
-
-// app.get('/date', function(request, response) {
-//     var date = new Date().toDateString();
-//     response.send(date);
-
-// });
 
 app.get('/cities', function(request, response) {
-    var cities = ['Providence', 'New Orleans', 'Omaha', 'Chicago'];
-    response.json(cities);
+    // var cities = ['Providence', 'New Orleans', 'Omaha', 'Chicago', 'San Francisco', 'New York City', 'Seattle', 'Boston'];
+    //add a query string parameter
+    if (request.query.limit > 0 && request.query.limit < length) { //only slice if limit request is within the object length
+        var sliced = Object.keys(cities).slice(0, request.query.limit); //must use Object.keys for slice() to work
+        response.json(sliced);
+    }
+    else if (request.query.limit > length) { //bad request error if limit exceeds object length
+        response.status(400).json('Your request exceeds the object length');
+    }
+    else {
+        response.json(Object.keys(cities)); //use Object.keys() to only show city names
+    }
 });
-// app.get('/', function(request, response) {
-//     response.sendFile(__dirname + '/public/index.html'); //links index.html file from public folder but not client.js
-// });
+
+//middleware function to normalize city name input
+app.param('state', function(request, response, next) {
+    var name = request.params.state;
+    var cityName = name[0].toUpperCase() + name.slice(1).toLowerCase(); //changes newOrleans to Neworleans, adjust later to create camelCase or accomodate spaces
+    request.cityName = cityName; //allows access from other routes in app
+    next();
+});
+
+
+//Use dynamic route to return state, 200 success code
+app.get('/cities/:state', function(request, response) {
+    var state = cities[request.cityName];
+    if (!state) { //return an error if no state is available
+        response.status(404).json('No entry found for ' + request.params.state);
+    }
+    else {
+        response.json(state);
+    }
+});
 
 app.use(express.static('public')); //middleware to link all files in public folder to browser call
 
