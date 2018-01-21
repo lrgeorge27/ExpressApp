847281b091ce0ea72fab8bc7a7f8fd0de8ecde56
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-21 13:11:18 +0000

    added notes to level 2 assignment

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ var app = express();
 //     response.send('Hello World');
 // });
 
-app.get('/name', function(request, response) {
-    var name = "Lauren George";
-    response.send(name);
-});
+// app.get('/name', function(request, response) {
+//     var name = "Lauren George";
+//     response.send(name);
+// });
 
-app.get('/redirect', function(request, response) {
-    // var redirect = "Surprise, I've moved!";
-    response.redirect(301, '/surprise');
-});
+// app.get('/redirect', function(request, response) {
+//     // var redirect = "Surprise, I've moved!";
+//     response.redirect(301, '/surprise');
+// });
 
-app.get('/date', function(request, response) {
-    var date = new Date().toDateString();
-    response.send(date);
+// app.get('/date', function(request, response) {
+//     var date = new Date().toDateString();
+//     response.send(date);
 
-});
+// });
 
 app.get('/cities', function(request, response) {
     var cities = ['Providence', 'New Orleans', 'Omaha', 'Chicago'];
     response.json(cities);
 });
 // app.get('/', function(request, response) {
-//     response.sendFile(__dirname + '/public/index.html');
+//     response.sendFile(__dirname + '/public/index.html'); //links index.html file from public folder but not client.js
 // });
 
-app.use(express.static('public')); //middleware to link index.html from public folder to browser call
+app.use(express.static('public')); //middleware to link all files in public folder to browser call
 
 app.listen(process.env.PORT, function() {
     console.log('Listening on env.PORT');
