a6038717ebdac6cfbf3aa13e21d963958a4ec58b
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-22 11:31:37 +0000

    updates HTML and client.js, POST call not functioning

--- public/client.js
+++ public/client.js
@@ -4,9 +4,27 @@ $(function() {
 
     function appendToList(cities) {
         var list = [];
+        var city, content;
         for (var i in cities) {
-            list.push($('<option>', { text: cities[i] }, '</option>'));
+            city = cities[i];
+            content = '<a href="/cities/' + city + '">' + city + '</a>';
+            list.push($('<li>', { html: content }, '</li>'));
         }
         $('.cities-list').append(list);
     }
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var form = $(this); //makes form easier to work with
+        var cityData = form.serialize(); //transforms form data to url-encoded notation
+
+        $.ajax({
+            type: 'POST',
+            url: '/cities',
+            data: cityData
+        }).done(function(cityName) {
+            appendToList([cityName]); //array with single argument, keeps function parameters consistant
+            form.trigger('rest'); //clears form text input fields
+        });
+    });
 });
