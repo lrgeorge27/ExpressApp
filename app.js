935bb1c9e8042929fbd6a2bf4715c3c9e8a5ae5e
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-22 18:36:56 +0000

    finished level 4, alert not working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser'); //parsing depends on middleware not shipped with Express
+var parseUrlEncoded = bodyParser.urlencoded({ extended: false }); //forces use of the native querystring Node library
+
 var cities = {
     'Providence': 'Rhode Island',
     'Omaha': 'Nebraska',
@@ -16,6 +19,22 @@ var cities = {
 
 var length = Object.keys(cities).length; //find the length of the object, needs global scope
 
+app.post('/cities', parseUrlEncoded, function(request, response) { //routes can take multiple handlers as arguments and call them sequentially
+    if (request.body.addCity.length > 4 || request.body.state.length > 2) {
+        var newCity = request.body;
+        cities[newCity.addCity] = newCity.state;
+        response.status(201).json(newCity.addCity);
+    }
+    else {
+        response.status(400).json("Invalid city or state.");
+        alert = "Invalid Entry";
+    }
+});
+
+app.delete('/cities/:addCity', function(request, response) {
+    delete cities[request.cityName]; //removes entry from object
+    response.sendStatus(200); //used when we don't set a response body
+});
 
 app.get('/cities', function(request, response) {
     // var cities = ['Providence', 'New Orleans', 'Omaha', 'Chicago', 'San Francisco', 'New York City', 'Seattle', 'Boston'];
@@ -46,6 +65,7 @@ app.get('/cities/:state', function(request, response) {
     var state = cities[request.cityName];
     if (!state) { //return an error if no state is available
         response.status(404).json('No entry found for ' + request.params.state);
+        // console.log("app.get 404");
     }
     else {
         response.json(state);
--- public/client.js
+++ public/client.js
@@ -7,16 +7,27 @@ $(function() {
         var city, content;
         for (var i in cities) {
             city = cities[i];
-            content = '<a href="/cities/' + city + '">' + city + '</a>';
+            content = '<a href="/cities/' + city + '">' + city + '</a>' + '<a href="#" data-city="' + city + '"> X </a>';
             list.push($('<li>', { html: content }, '</li>'));
         }
         $('.cities-list').append(list);
     }
 
+    function invalidEntry() {
+        if ($('#city').length < 4 || $('#state').length < 2) {
+            alert = "Invalid entry";
+            return false;
+        }
+    }
+
     $('form').on('submit', function(event) {
         event.preventDefault();
+        invalidEntry();
+
+
         var form = $(this); //makes form easier to work with
         var cityData = form.serialize(); //transforms form data to url-encoded notation
+
 
         $.ajax({
             type: 'POST',
@@ -24,7 +35,23 @@ $(function() {
             data: cityData
         }).done(function(cityName) {
             appendToList([cityName]); //array with single argument, keeps function parameters consistant
-            form.trigger('rest'); //clears form text input fields
+            form.trigger('reset'); //clears form text input fields
+        });
+
+        //Delete item from the list
+        $('.cities-list').on('click', '[data-city]', function(event) {
+            if (!confirm('Are you sure you want to delete this city?')) {
+                return false;
+            }
+
+            var target = $(event.currentTarget); //link element that was clicked
+
+            $.ajax({
+                type: 'DELETE',
+                url: '/cities/' + target.data('city') //reads name from links data attribute
+            }).done(function() {
+                target.parents('li').remove(); //removes li element containing link
+            });
         });
     });
 });
